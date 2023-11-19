6536118e57c6993c8cd38cd6816666b1135f029f
Morgan Metz <mlmi4@k12albemarle.org>
2023-11-19 16:48:42 -0500

    Update tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,6 @@
-use cosmwasm_std::{Addr, Uint128, Empty, coins};
+use std::str::FromStr;
+
+use cosmwasm_std::{Addr, Uint128, Decimal, Empty, coins};
 use cw_multi_test::{App, ContractWrapper, Executor};
 use nft_multi_test::{self, cw721_contract};
 
@@ -439,4 +441,26 @@ fn delist() {
         },
         &vec![]
     ).expect("could not delist");
+}
+
+#[test]
+fn cw_math_platform_fee() {
+    let payment = Uint128::new(1000000000000000000);
+    let fee = Decimal::percent(3);
+    let fee_amount = payment * fee;
+
+    assert_eq!(fee_amount, Uint128::new(30000000000000000));
+}
+
+#[test]
+fn cw_math_royalties() {
+    let payment = Uint128::new(1069000000000000000);
+    let price = Uint128::new(1000000000000000000);
+    let fee = Decimal::percent(3);
+    let royalties = Decimal::from_ratio(690 as u32, 10_000u32);
+    let royalty = payment - price;
+    let fee = price * fee;
+    assert_eq!(royalty, price * royalties);
+    assert_eq!(royalty, Uint128::new(69000000000000000));
+    assert_eq!(fee, Uint128::new(30000000000000000));
 }
